d23ac9d9b536a59b770132e9e21286dc572f2eed
projectbyd <project.by.deee@gmail.com>
2026-05-17 20:50:06 +0700

    Create FilterChip.tsx

---
+++ components/FilterChip.tsx
@@ -0,0 +1,9 @@
+export default function FilterChip({
+  label,
+}: any) {
+  return (
+    <div className="bg-white rounded-full px-4 py-2 text-[#4B3535] font-semibold">
+      {label}
+    </div>
+  );
+}
