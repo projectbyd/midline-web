054f1c8d1f95d45cb5302aad7e009e2f7fa4b53a
projectbyd <project.by.deee@gmail.com>
2026-05-17 20:49:22 +0700

    Create upcomingcard.tsx

---
+++ components/upcomingcard.tsx
@@ -0,0 +1,9 @@
+export default function UpcomingCard({
+  text,
+}: any) {
+  return (
+    <div className="bg-[#E7C58A] rounded-[24px] p-5 font-semibold text-[#4B3535]">
+      {text}
+    </div>
+  );
+}
